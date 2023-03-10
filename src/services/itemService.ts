14c446bb1a6f81b9ba60cde038c7067ef4b8a2ef
hpweisberg <hpweisberg@gmail.com>
2023-03-10 10:43:04 -0800

    add itemServices

--- src/services/itemService.ts
+++ src/services/itemService.ts
@@ -3,15 +3,16 @@ import * as tokenService from './tokenService'
 
 // types
 import { Item } from '../types/models'
+import { ItemFormData } from '../types/forms'
 
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/profiles`
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/items`
 
-async function getAllProfiles(): Promise<Profile[]> {
+async function index(): Promise<Item[]> {
   try {
     const res = await fetch(BASE_URL, {
       headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
     })
-    return await res.json() as Profile[]
+    return await res.json() as Item[]
   } catch (error) {
     throw error
   }
@@ -35,18 +36,72 @@ async function addPhoto(
   }
 }
 
-async function show(id:number): Promise<Profile> {
+async function show (id:number): Promise<Item> {
   try {
     const res = await fetch(`${BASE_URL}/${id}`, {
       headers: { 'Authorization': `Bearer ${tokenService.getToken()}`},
     })
-    return await res.json() as Profile
+    return await res.json() as Item
   } catch (error) {
     throw error
   }
 }
 
+async function create (itemData: ItemFormData): Promise<Item> {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: { 
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(itemData)
+    })
+    return await res.json() as Item
+  } catch (error) {
+    throw error
+  }
+}
+
+async function update (itemData: ItemFormData): Promise<Item> {
+  try {
+    const res = await fetch(`${BASE_URL}/${itemData.id}`, {
+      method: 'PUT',
+      headers: { 
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(itemData)
+    })
+    return await res.json() as Item
+  } catch (error) {
+    throw error
+  }
+}
+
+async function deleteItem (id: number): Promise<Item> {
+  try {
+    const res = await fetch(`${BASE_URL}/${id}`, {
+      method: 'DELETE',
+      headers: { 
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+      },
+    })
+    return await res.json() as Item
+  } catch (error) {
+    throw error
+  }
+}
+
+
+
+
+
 export { 
-  getAllProfiles, 
+  index, 
   addPhoto,
-  show }
+  show,
+  create,
+  update,
+  deleteItem as delete 
+}
